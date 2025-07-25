2d63c4ba79156a742432049b5ac6a0aef58b5c48
oslimani-oa <obeida.slimani@loreal.com>
2025-07-25 15:01:53 +0200

    Initial commit

---
+++ env_config.py
@@ -0,0 +1,6 @@
+import os
+
+os.environ['CLAUDE_CODE_USE_VERTEX'] = '1'
+os.environ['CLOUD_ML_REGION'] = 'us-east5'
+os.environ['ANTHROPIC_VERTEX_PROJECT_ID'] = 'oa-data-btdpexploration-np'
+os.environ['DISABLE_PROMPT_CACHING'] = '1'
